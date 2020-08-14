f7b772d76697a2bb0f3bfc58c3f73f92719090c9
Niklas Kiefer <niklas.kiefer@camunda.com>
2020-08-14 09:06:00 +0200

    chore(project): initial base demo

---
+++ packages/_base/src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  import AppContainer from './components/AppContainer';
+  import Button from './components/Button';
+
+  import './App.scss';
+
+  const handleButtonClick = () => {
+    alert('The button has been clicked!');
+  };
+
+</script>
+
+<div class="app">
+  <AppContainer>
+    <Button onClick={handleButtonClick} />
+  </AppContainer>
+</div>
